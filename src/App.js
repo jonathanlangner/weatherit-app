87112b10d387a6f17af44a3dc555c7c5b8df5163
jonathanlangner <jonlangner@gmail.com>
2022-11-09 22:22:19 -0600

    Added base styling and layout of the weather app.
    Added base styling and layout of the weather app.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useState } from 'react';
+import "./css/app.css";
+
 const App = function() {
+  const [value, empty] = useState("Search for Town, or City");
+  
+  const handleChange = event => {
+    empty(event.target.value);
+  };
+  const handleEmpty = function(){
+    empty("");
+  }
+
   return (
     <div className="App">
-    
+    <div className="page-title-div"><h1 className="page-title">❄️ Audacious Weather ❄️</h1>
+      <p className="description">Adventurous Weather Fun Every Day.</p>
+    </div>
+      <form>
+          <input type="text" value={value} onClick={handleEmpty} onChange={handleChange} className="search-text-box" title="Search for Town, City, or Airport."
+           />
+        <input type="submit" value="Search" className="search-btn" title="Search" />
+      </form>
     </div>
   );
 }
